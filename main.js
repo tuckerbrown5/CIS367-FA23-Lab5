f741fe3d26fa03559889462490602b2842e8a2ef
Kenan Casey <kenancasey@gmail.com>
2023-10-30 23:30:54 -0500

    progress
    But it's not pretty.

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
 
+const data = ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"];
+
+const carousel = document.getElementsByClassName("carousel")[0];
+var activeIndex = Math.floor(data.length/2);
+
+
+function renderCards() {
+
+    carousel.innerHTML = "";
+    var windowWidth = window.innerWidth;
+    console.log(windowWidth);
+    var cardWidth = 350;
+    
+    data.forEach( (data, index) => {
+        const length = data.length;
+        let div = document.createElement('div');
+        div.classList.add("box");
+    
+        if( index < activeIndex){
+            div.classList.add("left");
+            const offset = windowWidth/2 - cardWidth/2 - index * 10;
+            div.style.transform = `translateX(${-offset}px)`;
+            //div.style.left = `${index*10}px`;
+            //div.style.transform+=` scale(${ Math.pow(0.9, length-index+1)})`;
+        }
+        else if(index === activeIndex)
+        {
+            div.classList.add("active");
+        }
+        else {
+            div.classList.add("right");
+            const offset = windowWidth/2 - cardWidth/2 + (length - index+1) * 10;
+            console.log(offset)
+            div.style.transform = `translateX(${offset}px)`;
+        }
+    
+        div.innerHTML = `${index} ${data}`
+    
+        carousel.appendChild(div);
+    
+    });
+}
+
+renderCards();
+
+window.addEventListener("resize", renderCards);
+
+
+document.getElementById("prevButton").addEventListener("click", ()=>{
+    activeIndex--;
+    renderCards();
+});
+
+document.getElementById("nextButton").addEventListener("click", ()=>{
+    activeIndex++;
+    renderCards();
+});
