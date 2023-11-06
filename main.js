59166bb1ee24b7eee930a8f48d9f0d641bea1c1a
Kenan Casey <kenancasey@gmail.com>
2023-11-06 14:47:50 -0600

    fixes

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function renderCards() {
     console.log(windowWidth);
     var cardWidth = 350;
     
-    data.forEach( (data, index) => {
+    data.forEach( (item, index) => {
         const length = data.length;
         let div = document.createElement('div');
         div.classList.add("box");
@@ -30,19 +30,55 @@ function renderCards() {
         }
         else {
             div.classList.add("right");
-            const offset = windowWidth/2 - cardWidth/2 + (length - index+1) * 10;
+            const offset = windowWidth/2 - cardWidth/2 - (length - index+1) * 10;
             console.log(offset)
             div.style.transform = `translateX(${offset}px)`;
+            div.style.zIndex = (length - index);
+            div.style.right  = `${offset}px`
         }
     
-        div.innerHTML = `${index} ${data}`
+        div.innerHTML = `${index} ${item}`
     
         carousel.appendChild(div);
-    
     });
 }
 
 renderCards();
+
+function updateCards() {
+
+    carousel.innerHTML = "";
+    var windowWidth = window.innerWidth;
+    console.log(windowWidth);
+    var cardWidth = 350;
+    
+    data.forEach( (item, index) => {
+        const length = data.length;
+        let div = document.createElement('div');
+        div.classList.add("box");
+    
+        if( index < activeIndex){
+            div.classList.add("left");
+            const offset = windowWidth/2 - cardWidth/2 - index * 10;
+            div.style.transform = `translateX(${-offset}px)`;
+            //div.style.left = `${index*10}px`;
+            //div.style.transform+=` scale(${ Math.pow(0.9, length-index+1)})`;
+        }
+        else if(index === activeIndex)
+        {
+            div.classList.add("active");
+        }
+        else {
+            div.classList.add("right");
+            const offset = windowWidth/2 - cardWidth/2 - (length - index+1) * 10;
+            console.log(offset)
+            div.style.transform = `translateX(${offset}px)`;
+            div.style.zIndex = (length - index);
+            //div.style.right  = `${offset}px`
+        }
+    });
+
+}
 
 window.addEventListener("resize", renderCards);
 
