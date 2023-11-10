0bfa809e5b87753cab14b0ab90890a7d3dd6a5a3
tuckerbrown5 <tucker.brown@students.fhu.edu>
2023-11-10 14:19:26 -0600

    updated design

--- main.js
+++ main.js
@@ -8,38 +8,34 @@ document.addEventListener('DOMContentLoaded', function() {
         let div = document.createElement('div');
         div.classList.add("box");
         div.innerHTML = `
-            <div class="grid-individual-card card-background-yellow">
-                <div class="header-basic-title">${person.Rank}</div>
-                <div class="header-profile-name">${person.FirstName} ${person.LastName}</div>
-                <div class="header-hp">${person.HitPoints} HP</div>
-                <div class="header-icon">☻</div>
-                <img src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image}" alt="${person.FirstName} ${person.LastName}">
-                <div class="description-below-image">
-                    <span class="description-below-image-background">${person.Department} Type-${person.Type}, ${person.EducationLevel} in ${person.FieldofStudy}</span>
-                </div>
-                <div class="skill-cost">☻ ☻</div>
-                <div class="skill-description skill-center"><span class="skill-name">${person.Attack1}</span></div>
-                <div class="skill-damage">${person.Attack1Damage}</div>
-                <div class="skill-cost">☻ ☻ ☻</div>
-                <div class="skill-description">
-                    <span class="skill-name">${person.Attack2}</span> Flip a coin. If tails, ${person.FirstName} does 10 damage to themselves.
-                </div>
-                <div class="skill-damage">${person.Attack2Damage}</div>
-                <div class="weakness wrr-header">Weakness</div>
-                <div class="resistance wrr-header">Resistance</div>
-                <div class="retreat wrr-header">Retreat Cost</div>
-                <div class="weakness wrr-value">☻</div>
-                <div class="resistance wrr-value">${person.Resistances || ""}</div>
-                <div class="retreat wrr-value">${"☻ ".repeat(person.RetreatCost)}</div>
-                <div class="description-above-footer">
-                    <ul>
-                        <li class="description-above-footer-border">${person.FirstName} ${person.LastName}, known for the hashtag ${person.HashTag}. Resistant to ${person.Resistances}, weak to ${person.Weaknesses}. LV. ${person.Level} ${person.Gender}</li>
-                    </ul>
-                </div>
-                <div class="copyright">
-                    <strong>● Tucker Brown</strong> ©2023, CIS367. <strong>${person.ID}/59 ●</strong>
-                </div>
-            </div>
+        <div class="grid-individual-card card-background-yellow">
+        <!-- BEGIN Casey -->
+        <div class="header-basic-title">${person.Rank}</div>
+        <div class="header-profile-name">${person.FirstName} ${person.LastName}</div>
+        <div class="header-hp">${person.HitPoints}HP</div>
+        <div class="header-icon">☻</div>
+        <img src="https://fhu-faculty-api.netlify.app/images/headshots/${person.Image} " alt="${person.FirstName} ${person.LastName}">
+        <div class="description-below-image-empty"><!-- empty cell to position description correctly --></div>
+        <div class="description-below-image"><span class="description-below-image-background">${person.Department} Type-${person.Type}, ${person.EducationLevel} in ${person.FieldofStudy}</span></div>
+        <div class="description-below-image-empty"><!-- empty cell to position description correctly --></div>
+        <div class="skill-cost">☻ ☻</div>
+        <div class="skill-description skill-center"><span class="skill-name">${person.Attack1}</span></div>
+        <div class="skill-damage">${person.Attack1Damage}</div>
+        <div class="skill-cost">☻ ☻ ☻</div>
+        <div class="skill-description"><span class="skill-name">${person.Attack2}</span> Flip a coin. If tails, ${person.FirstName} does 10 damage to themself.</div>
+        <div class="skill-damage">${person.Attack2Damage}</div>
+        <div class="weakness wrr-header">weakness</div>
+        <div class="resistance wrr-header">resistance</div>
+        <div class="retreat wrr-header">retreat cost</div>
+        <div class="weakness wrr-value">☻</div>
+        <div class="resistance wrr-value"></div>
+        <div class="retreat wrr-value">☻ ☻</div>
+        <div class="description-above-footer">
+          <ul>
+            <li class="description-above-footer-border">${person.FirstName} ${person.LastName}, known for the hashtag ${person.HashTag}. He has a resistance of ${person.Resistances}, but weak to ${person.Weaknesses} LV. ${person.Cost} &nbsp; ${person.Gender}</li>
+          </ul>
+        </div>
+        <div class="copyright"><strong>● Tucker Brown</strong> ©2023, CIS367. <strong>${person.id}/59 ●</strong></div>
         `;
         return div;
     }
@@ -47,6 +43,9 @@ document.addEventListener('DOMContentLoaded', function() {
     async function addAllCards() {
         try {
             let response = await fetch(API_URL);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
             people = await response.json();
 
             people.forEach((person, index) => {
@@ -60,6 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
             updateCards();
         } catch (error) {
             console.error("Failed to fetch and create cards:", error);
+            // Optionally, handle user-facing error reporting here
         }
     }
 
